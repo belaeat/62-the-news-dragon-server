8148b972a08928334960f8ded14c98761d2e8106
belaeat <omyhossain@gmail.com>
2023-05-03 03:23:46 +0600

    all data loaded

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 
 const categories = require('./data/categories.json')
 
+const news = require('./data/news.json')
+
 app.use(cors())
 
 
@@ -13,11 +15,42 @@ app.get('/', (req, res) => {
     res.send("Dragon is Running")
 });
 
-app.get('/categories', (req, res) =>{
+// Category data load
+
+app.get('/categories', (req, res) => {
     res.send(categories);
 })
 
+// News data load
 
-app.listen(port, ()=>{
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+// News er id theke find
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
+// Category wise filter
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews)
+    }
+
+})
+
+
+
+app.listen(port, () => {
     console.log(`Dragon API is running on port: ${port}`)
 })
